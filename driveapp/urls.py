a2eaf0556be600262fb586b81d1f06554fd49e39
BhawNo <2017chb1038@iitrpr.ac.in>
2018-06-21 22:14:04 +0530

    Modified templates to display posts from database.

--- driveapp/urls.py
+++ driveapp/urls.py
@@ -1,8 +1,8 @@
 
 from django.conf.urls import url, include
-from . import views
+from .import views
 urlpatterns = [
 
     
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.post_list, name='post_list'),
 ]
